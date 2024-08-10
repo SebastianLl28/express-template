61e25e91f07a49c8de9387bf37c75591206e6bf4
Llamuca Dueñas Sebastián <llamucasebas@gmail.com>
2024-08-10 09:32:55 -0500

    chore: Update Jest configuration to ignore coverage for 'prisma' and 'dist' directories

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,9 @@
 module.exports = {
   testEnvironment: 'node',
   preset: 'ts-jest',
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts']
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  coveragePathIgnorePatterns: [
+    'prisma',
+    'dist'
+  ]
 }
